3bc1036d91d9100099d84025365a8adb5b63c387
Vladyslav Korniienko <maradonna624@gmail.com>
2024-04-22 19:46:45 +0300

    Add AddProduct method for Products

--- src/app/core/services/products.service.ts
+++ src/app/core/services/products.service.ts
@@ -7,29 +7,44 @@ import {
   HttpErrorResponse,
 } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { Product } from '@shared/models/Product/product';
+import { API_ENDPOINTS } from '@shared/constants/api-endpoints';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductsService {
-  endpoint: string = 'https://localhost:7218/api/Products';
   headers = new HttpHeaders().set('Content-Type', 'application/json');
-  constructor(private http: HttpClient, public router: Router) { }
+  constructor(private http: HttpClient, public router: Router) {}
 
   // Get all Products
-  getProducts(): Observable<any> {
-    let api = `${this.endpoint}`;
-    return this.http.get(api, { headers: this.headers }).pipe(
-      map((res) => {
-        return res || {};
-      }),
-      catchError(this.handleError)
-    );
+  getProducts(): Observable<Array<Product>> {
+    return this.http
+      .get<Array<Product>>(API_ENDPOINTS.products, { headers: this.headers })
+      .pipe(
+        map((products: any[]) =>
+          products.map(
+            (product) =>
+              new Product(
+                product.id,
+                product.name,
+                product.priceUSD,
+                product.genreName, // Get the name from the genre object
+                product.categoryName, // Get the name from the category object
+                product.description,
+                product.icon,
+                product.screenshots
+              )
+          )
+        ),
+        catchError(this.handleError)
+      );
   }
   // Post Product
   addProduct(product: any): Observable<any> {
-    let api = `${this.endpoint}`;
-    return this.http.post(api, product).pipe(catchError(this.handleError));
+    return this.http
+      .post(API_ENDPOINTS.products, product)
+      .pipe(catchError(this.handleError));
   }
 
   // Error
