8c047129416eebc21367a31ed3f70bfba9b87b38
Vladyslav Korniienko <maradonna624@gmail.com>
2024-04-22 19:42:54 +0300

    Add Passwprd change for User

--- src/app/core/components/user/user-profile/user-profile.component.ts
+++ src/app/core/components/user/user-profile/user-profile.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { PasswordInfo } from '@shared/models/Auth/password-info';
 import { User } from '@shared/models/User/user';
+import { AuthService } from 'src/app/core/services/auth.service';
 import { UserService } from 'src/app/core/services/user.service';
 
 @Component({
@@ -10,14 +13,37 @@ import { UserService } from 'src/app/core/services/user.service';
 })
 export class UserProfileComponent implements OnInit {
   currentUser: User = new User();
+  changePasswordForm!: FormGroup;
   constructor(
+    public fb: FormBuilder,
     public userService: UserService,
+    public authService: AuthService,
     private actRoute: ActivatedRoute
   ) {
+    this.changePasswordForm = this.fb.group({
+      oldPasswordInput: [''],
+      newPasswordInput: [''],
+    })
+  }
+  ngOnInit() {
     let id = this.actRoute.snapshot.paramMap.get('id') || '';
     this.userService.getUser(id).subscribe((res: User) => {
       this.currentUser = res;
     });
   }
-  ngOnInit() { }
+  changePassword() {
+
+    let passwordInfo = new PasswordInfo();
+    passwordInfo.oldPassword = this.changePasswordForm.value.oldPasswordInput;
+    passwordInfo.newPassword = this.changePasswordForm.value.newPasswordInput;
+
+    this.authService.changePassword(passwordInfo).subscribe(
+      data => {
+        //add mat snack
+      },
+      error => {
+        //add mat snack
+      }
+    )
+  }
 }
