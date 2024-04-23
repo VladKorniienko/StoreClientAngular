01c09e2975fb5cf54b2743ba7a8df21a1057f226
Vladyslav Korniienko <maradonna624@gmail.com>
2024-04-23 22:31:22 +0300

    Add user library

---
+++ src/app/core/components/library/library.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '@shared/models/Product/product';
+import { UserService } from '../../services/user.service';
+import { MatDialog } from '@angular/material/dialog';
+import { HomeInfoDialogComponent } from '../home-info-dialog/home-info-dialog.component';
+
+@Component({
+  selector: 'app-library',
+  templateUrl: './library.component.html',
+  styleUrls: ['./library.component.css'],
+})
+export class LibraryComponent implements OnInit {
+  public userProducts: Array<Product>;
+  constructor(public userService: UserService, public dialog: MatDialog) {
+    this.userProducts = new Array<Product>();
+  }
+
+  openInfoDialog(product: Product) {
+    this.dialog.open(HomeInfoDialogComponent, {
+      data: product,
+      height: '600px',
+      width: '800px',
+    });
+  }
+
+  ngOnInit(): void {
+    let userId = this.userService.getUserId();
+    this.userService.getUser(userId).subscribe((res) => {
+      this.userProducts = res.products;
+    });
+  }
+}
