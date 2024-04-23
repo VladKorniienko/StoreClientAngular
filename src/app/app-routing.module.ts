ea3bd8375a3672ab0c368d2da137d2ef11bc1e49
Vladyslav Korniienko <maradonna624@gmail.com>
2024-04-23 16:52:06 +0300

    Add logged-in guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,19 +3,32 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './core/components/login-page/login-page.component';
 import { SignupPageComponent } from './core/components/signup-page/signup-page.component';
 import { HomeComponent } from './core/components/home/home.component';
-import { AuthGuard } from "./shared/guards/auth.guard";
-import { UserProfileComponent } from "./core/components/user/user-profile/user-profile.component";
+import { AuthGuard } from './shared/guards/auth.guard';
+import { UserProfileComponent } from './core/components/user/user-profile/user-profile.component';
+import { LoggedInGuard } from '@shared/guards/logged-in.guard';
 
 const routes: Routes = [
-{ path:"", redirectTo:"login", pathMatch:"full"},
-{ path: 'login', component: LoginPageComponent },
-{ path: 'signup', component: SignupPageComponent },
-{ path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
-{ path: 'users/:id', component: UserProfileComponent, canActivate: [AuthGuard] }
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  {
+    path: 'login',
+    component: LoginPageComponent,
+    canActivate: [LoggedInGuard],
+  },
+  {
+    path: 'signup',
+    component: SignupPageComponent,
+    canActivate: [LoggedInGuard],
+  },
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  {
+    path: 'users/:id',
+    component: UserProfileComponent,
+    canActivate: [AuthGuard],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/shared/guards/logged-in.guard.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  CanActivateFn,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/core/services/auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoggedInGuard {
+  constructor(public authService: AuthService, public router: Router) {}
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    if (this.authService.isLoggedIn === true) {
+      window.alert('You are already logged in!');
+      this.router.navigate(['home']);
+      return false;
+    }
+    return true;
+  }
+}
