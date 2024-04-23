69df3ed1edd5143ca9fe77aad02a83e9be7d1f93
Vladyslav Korniienko <maradonna624@gmail.com>
2024-04-23 22:31:36 +0300

    Add role guard

---
+++ src/app/shared/guards/role.guard.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivateFn,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/core/services/auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RoleGuard {
+  private expectedRole: string = 'Admin';
+  constructor(public authService: AuthService, public router: Router) {}
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    if (this.authService.getUserRole() !== this.expectedRole) {
+      window.alert('Access denied!');
+      this.router.navigate(['home']);
+      return false;
+    }
+    return true;
+  }
+}
